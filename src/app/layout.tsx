b2f32366aefc58dee534c2f00d4636474f1ce238
henriwasd <henriwasd@gmail.com>
2024-06-06 00:47:43 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+
+import { TRPCReactProvider } from "~/trpc/react";
+
+export const metadata = {
+  title: "Verde Rápido",
+  description: "Delivery do Verde",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt" className={`${GeistSans.variable}`}>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
